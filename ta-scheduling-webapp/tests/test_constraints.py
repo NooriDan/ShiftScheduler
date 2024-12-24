202e0222e7c808dc0055642d3c84326d0f249275
Danial Noori Zadeh <noorizad@mcmaster.ca>
2024-12-24 14:20:47 -0500

    testcases under construction

--- ta-scheduling-webapp/tests/test_constraints.py
+++ ta-scheduling-webapp/tests/test_constraints.py
@@ -6,37 +6,28 @@ from employee_scheduling.constraints import *
 from datetime import date, datetime, time, timedelta
 
 DAY_1 = date(2021, 2, 1)
+DAY_2 = date(2021, 2, 2)
 DAY_3 = date(2021, 2, 3)
+DAY_4 = date(2021, 2, 4)
+DAY_5 = date(2021, 2, 5)
+
 DAY_START_TIME = datetime.combine(DAY_1, time(9, 0))
 DAY_END_TIME = datetime.combine(DAY_1, time(17, 0))
+
 AFTERNOON_START_TIME = datetime.combine(DAY_1, time(13, 0))
 AFTERNOON_END_TIME = datetime.combine(DAY_1, time(21, 0))
 
 constraint_verifier = ConstraintVerifier.build(define_constraints, Timetable, ShiftAssignment)
 
-
-def test_required_skill():
-    employee = Employee(name="Amy")
-    (constraint_verifier.verify_that(required_skill)
-    .given(employee,
-           Shift(id="1", start=DAY_START_TIME, end=DAY_END_TIME, location="Location", required_skill="Skill", employee=employee))
-    .penalizes(1))
-    
-    employee = Employee(name="Beth", skills={"Skill"})
-    (constraint_verifier.verify_that(required_skill)
-    .given(employee,
-           Shift(id="2", start=DAY_START_TIME, end=DAY_END_TIME, location="Location", required_skill="Skill", employee=employee))
-    .penalizes(0))
-
+# Mock Shifts
+shift1 = Shift(name="L01", required_tas=1)
+shift2 = Shift(name="L02", required_tas=2)
+shift3 = Shift(name="L03", required_tas=1)
+shift4 = Shift(id="4", series="L04", day_of_week="Wed", start_time=)
 
 # Mock TAs
-ta1 = TA(name="TA1", required_shifts=2, unavailable=["Shift1"], undesired=["Shift2"], desired=["Shift3"])
-ta2 = TA(name="TA2", required_shifts=3, unavailable=["Shift2"], undesired=["Shift1"], desired=["Shift4"])
-
-# Mock Shifts
-shift1 = Shift(name="Shift1", required_tas=1)
-shift2 = Shift(name="Shift2", required_tas=2)
-shift3 = Shift(name="Shift3", required_tas=1)
+ta1 = TA(name="TA1", required_shifts=2, unavailable=[shift1], undesired=[shift2], desired=[shift3])
+ta2 = TA(name="TA2", required_shifts=3, unavailable=[shift2], undesired=[shift1], desired=[shift4])
 
 # Mock Shift Assignments
 assignments = [
@@ -44,6 +35,7 @@ assignments = [
     ShiftAssignment(shift=shift2, assigned_tas=[ta2]),  # Matches required TAs
     ShiftAssignment(shift=shift3, assigned_tas=[ta1, ta2])  # Extra TA assigned
 ]
+
 
 def test_required_skill():
       ta1 = TA(name="TA1", required_shifts=2, unavailable=["Shift1"], undesired=["Shift2"], desired=["Shift3"])
