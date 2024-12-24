ccb7128b083cd84200e96d2944ebcb42babf0be9
Aditya Rao <82911340+adityarao2005@users.noreply.github.com>
2024-12-24 03:33:30 +0000

    last but not least.. modify demo_data.py and rest api

--- ta-scheduling-webapp/src/employee_scheduling/solver.py
+++ ta-scheduling-webapp/src/employee_scheduling/solver.py
@@ -1,21 +1,39 @@
 from timefold.solver        import SolverManager, SolverFactory, SolutionManager
 from timefold.solver.config import (SolverConfig, ScoreDirectorFactoryConfig,
                                     TerminationConfig, Duration)
+from timefold.solver.score  import ConstraintFactory, Constraint
 
-from .domain        import EmployeeSchedule, Shift
-from .constraints   import define_constraints
+from .domain        import Timetable, ShiftAssignment
+from .constraints   import (basic_ta_scheduling_constraints, grad_undergrad_constraints, favourite_partners_constraints, all_constraints)
+from typing import Callable
 
 
-solver_config = SolverConfig(
-    solution_class    = EmployeeSchedule,
-    entity_class_list = [Shift],
-    score_director_factory_config    = ScoreDirectorFactoryConfig(
-        constraint_provider_function = define_constraints
-    ),
-    termination_config=TerminationConfig(
-        spent_limit=Duration(seconds=30)
-    )
-)
+class Solver:
+    def __init__(self, define_constraints: Callable[[ConstraintFactory], list[Constraint]]):
+        self.solver_config   = SolverConfig(
+            solution_class    = Timetable,
+            entity_class_list = [ShiftAssignment],
+            score_director_factory_config    = ScoreDirectorFactoryConfig(
+                constraint_provider_function = define_constraints
+            ),
+            termination_config=TerminationConfig(
+                spent_limit=Duration(seconds=30)
+            )
+        )
 
-solver_manager   = SolverManager.create(SolverFactory.create(solver_config))
-solution_manager = SolutionManager.create(solver_manager)
+        self.solver_manager   = SolverManager.create(SolverFactory.create(self.solver_config))
+        self.solution_manager = SolutionManager.create(self.solver_manager)
+
+    def solve(self, timetable: Timetable) -> Timetable:
+        return self.solution_manager.solve(timetable)
+    
+    def terminate(self):
+        self.solver_manager.terminate_early()
+    
+    def is_terminated(self) -> bool:
+        return self.solver_manager.is_termination_imminent()
+
+basic_ta_scheduling_solver = Solver(basic_ta_scheduling_constraints)
+grad_undergrad_solver = Solver(grad_undergrad_constraints)
+favourite_partners_solver = Solver(favourite_partners_constraints)
+all_constraints_solver = Solver(all_constraints)
