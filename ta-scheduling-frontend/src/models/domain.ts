210dc13dad71c40496ad0d3162e783d001998cef
Aditya Rao <82911340+adityarao2005@users.noreply.github.com>
2024-12-27 18:41:04 +0000

    working on UI part.. done all the model config stuff. now to create the components

---
+++ ta-scheduling-frontend/src/models/domain.ts
@@ -0,0 +1,89 @@
+export type DayOfWeek = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday" | "Sunday"
+
+export class Shift {
+    id: string
+    series: string
+    day_of_week: DayOfWeek
+    start_time: Date
+    end_time: Date
+    required_tas: number
+    // Optional
+    alias: string = "DEFAULT"
+    shift_date: Date = new Date(1900, 1, 1)
+
+    constructor(id: string, series: string, day_of_week: DayOfWeek, start_time: Date, end_time: Date, required_tas: number, alias: string = "DEFAULT", shift_date: Date = new Date(1900, 1, 1)) {
+        this.id = id
+        this.series = series
+        this.day_of_week = day_of_week
+        this.start_time = start_time
+        this.end_time = end_time
+        this.required_tas = required_tas
+        this.alias = alias
+        this.shift_date = shift_date
+    }
+}
+
+export class TA {
+    id: string
+    name: string
+    required_shifts: number
+    desired: Shift[] = []
+    undesired: Shift[] = []
+    unavailable: Shift[] = []
+    // favourite_partners: TA[] = None
+    is_grad_student: boolean = true
+
+    constructor(id: string, name: string, required_shifts: number, desired: Shift[] = [], undesired: Shift[] = [], unavailable: Shift[] = [], is_grad_student: boolean = true) {
+        this.id = id
+        this.name = name
+        this.required_shifts = required_shifts
+        this.desired = desired
+        this.undesired = undesired
+        this.unavailable = unavailable
+        this.is_grad_student = is_grad_student
+    }
+
+    get_status_for_shift(shift: Shift): string {
+        if (this.desired.includes(shift))
+            return 'Desired'
+        if (this.undesired.includes(shift))
+            return 'Undesired'
+        if (this.unavailable.includes(shift))
+            return 'Unavailable'
+        return 'Neutral'
+    }
+
+    is_available_for_shift(shift: Shift): boolean {
+        return !this.unavailable.includes(shift)
+    }
+}
+
+export class ShiftAssignment {
+    id: string
+    assigned_ta: TA
+    shift: Shift
+
+    constructor(id: string, assigned_ta: TA, shift: Shift) {
+        this.id = id
+        this.assigned_ta = assigned_ta
+        this.shift = shift
+    }
+}
+
+export class Timetable {
+    id: string
+    // problem facts
+    shifts: Shift[]
+    tas: TA[]
+    // planning entities
+    shift_assignments: ShiftAssignment[]
+    score: number
+
+    constructor(id: string, shifts: Shift[], tas: TA[], shift_assignments: ShiftAssignment[], score: number) {
+        this.id = id
+        this.shifts = shifts
+        this.tas = tas
+        this.shift_assignments = shift_assignments
+        this.score = score
+    }
+}
