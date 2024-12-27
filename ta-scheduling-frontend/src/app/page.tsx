16cf1e486db961c1b32ba37c201820b9691c60bc
Aditya Rao <82911340+adityarao2005@users.noreply.github.com>
2024-12-27 20:44:31 +0000

    modularized it

--- ta-scheduling-frontend/src/app/page.tsx
+++ ta-scheduling-frontend/src/app/page.tsx
@@ -1,13 +1,6 @@
-import Image from "next/image";
+import SchedulerGrid from "@/components/scheduler-grid";
 
 export default function Home() {
-  const times = []
-
-  for (let i = 8; i <= 20; i++) {
-    times.push(i)
-  }
-
-
   return (
     <div className="flex flex-1 flex-col p-4">
       <div className="">
@@ -17,34 +10,7 @@ export default function Home() {
 
 
       </div>
-      <div className="grid grid-cols-6">
-        <div className="col-start-2 row-start-1">Monday</div>
-        <div className="col-start-3 row-start-1">Tuesday</div>
-        <div className="col-start-4 row-start-1">Wednesday</div>
-        <div className="col-start-5 row-start-1">Thursday</div>
-        <div className="col-start-6 row-start-1">Friday</div>
-        {
-          times.map((time, index, arr) =>
-            <>
-              <div className={`col-start-1 row-start-${2 * index + 2}`}>{time == 12 ? 12 : time % 12}:30 {time / 12 < 1 ? "AM" : "PM"}</div>
-              <div className={`col-start-2 col-span-5 row-start-${2 * index + 2} flex flex-col`}>
-                <div className="flex-1"></div>
-                <hr className="bg-black h-1" />
-                <div className="flex-1"></div>
-              </div>
-              {
-                index < arr.length - 1 &&
-                <>
-                  <div className={`col-start-1 row-start-${2 * index + 3}`}>-</div>
-                  <div className={`col-start-2 row-start-${2 * index + 3}`}>-</div>
-                  <div className={`col-start-3 row-start-${2 * index + 3}`}>-</div>
-                  <div className={`col-start-4 row-start-${2 * index + 3}`}>-</div>
-                  <div className={`col-start-5 row-start-${2 * index + 3}`}>-</div>
-                </>}
-            </>
-          )
-        }
-      </div>
+      <SchedulerGrid />
     </div>
   );
 }
---
+++ ta-scheduling-frontend/src/components/scheduler-grid.tsx
@@ -0,0 +1,70 @@
+
+
+const times: number[] = []
+
+for (let i = 8; i <= 20; i++) {
+    times.push(i)
+}
+
+function BlockDivider({ row, time }: { row: number, time: number }) {
+    return (
+        <>
+            <div className={`col-start-1 row-start-${row} flex flex-row`}>
+                <div>
+                    {time == 12 ? 12 : time % 12}:30 {time / 12 < 1 ? "AM" : "PM"}
+                </div>
+                <hr />
+            </div>
+            <div className={`col-start-2 col-span-5 row-start-${row} flex flex-col`}>
+                <div className="flex-1"></div>
+                <hr className="bg-black h-1" />
+                <div className="flex-1"></div>
+            </div>
+        </>
+    )
+}
+
+function Block({ row, col }: { row: number, col: number }) {
+    return (
+        <div className={`col-start-${col} col-span-1 row-start-${row} h-10`}>
+        </div>
+    )
+}
+
+function Header() {
+    return (
+        <>
+            <div className="col-start-1 row-start-1">Time</div>
+            <div className="col-start-2 row-start-1">Monday</div>
+            <div className="col-start-3 row-start-1">Tuesday</div>
+            <div className="col-start-4 row-start-1">Wednesday</div>
+            <div className="col-start-5 row-start-1">Thursday</div>
+            <div className="col-start-6 row-start-1">Friday</div></>
+    )
+}
+
+function BlockRow({ index, time }: { index: number, time: number }) {
+    return (
+        <>
+            <BlockDivider row={2 * index + 2} time={time} />
+            {
+                index < times.length - 1 &&
+                <>
+                    <Block row={2 * index + 3} col={2} />
+                    <Block row={2 * index + 3} col={3} />
+                    <Block row={2 * index + 3} col={4} />
+                    <Block row={2 * index + 3} col={5} />
+                    <Block row={2 * index + 3} col={6} />
+                </>
+            }</>
+    )
+}
+
+export default function SchedulerGrid() {
+    return (<div className="grid grid-cols-6 bg-gray-300">
+        <Header />
+        {
+            times.map((time, index, arr) => <BlockRow key={index} index={index} time={time} />)
+        }
+    </div>)
+}
